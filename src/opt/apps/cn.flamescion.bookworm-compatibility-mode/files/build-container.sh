d013671b4fef75cb31f04886afea248b6aa608bf
shenmo <jifengshenmo@outlook.com>
2023-08-26 23:07:00 +0800

    fix: chown

--- src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
+++ src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
@@ -6,5 +6,7 @@ fi
 
 cd "`dirname $0`"
 sudo debootstrap bookworm ./bookworm-env https://mirrors.ustc.edu.cn/debian/
+sudo chown -R `whoami` ./bookworm-env
+rm -rf bookworm-env/var/cache/apt/archives/*.deb
 tar -I 'xz -T0' -cvf bookworm-env.tar.xz bookworm-env/*
 sudo rm -rf bookworm-env
