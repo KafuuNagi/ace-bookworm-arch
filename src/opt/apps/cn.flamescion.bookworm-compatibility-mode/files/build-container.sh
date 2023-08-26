a7c567be4376f66077f455e41968f49939a30431
shenmo <jifengshenmo@outlook.com>
2023-08-26 23:01:18 +0800

    Now enable multiple thread compress

--- src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
+++ src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
@@ -6,5 +6,5 @@ fi
 
 cd "`dirname $0`"
 sudo debootstrap bookworm ./bookworm-env https://mirrors.ustc.edu.cn/debian/
-tar -cJvf bookworm-env.tar.xz bookworm-env/*
+tar -I 'xz -T0' -cvf bookworm-env.tar.xz bookworm-env/*
 sudo rm -rf bookworm-env
