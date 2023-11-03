0d0085187f80598e5edaa30fd681727d0a3d9686
shenmo <jifengshenmo@outlook.com>
2023-11-03 12:19:37 +0800

    支持构建多种架构

--- src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
+++ src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
@@ -3,9 +3,19 @@ if [ `which debootstrap` = "" ];then
 echo "Need to install debootstrap!"
 exit
 fi
+if [ "$1" = "amd64" ] || [ "$1" = "x64" ];then
+ARCH="amd64"
+ARCH_ANOTHERWAY="x64"
+elif [ "$1" = "arm64" ] || [ "$1" = "arm" ];then
+ARCH="arm64"
+ARCH_ANOTHERWAY="arm64"
+else
+echo "Invalid architecture! Exit"
+exit 1
+fi
 
 cd "`dirname $0`"
-sudo debootstrap bookworm ./bookworm-env https://mirrors.ustc.edu.cn/debian/
+sudo debootstrap --arch=`${ARCH}` bookworm ./bookworm-env https://mirrors.ustc.edu.cn/debian/
 sudo chown -R `whoami` ./bookworm-env
 rm -rf bookworm-env/var/cache/apt/archives/*.deb
 tar -I 'xz -T0' -cvf bookworm-env.tar.xz bookworm-env/*
