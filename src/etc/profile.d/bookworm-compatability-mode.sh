b667c89c2d5857eb6ed89b047f4070a22c6aee1d
shenmo <jifengshenmo@outlook.com>
2023-11-09 18:11:51 +0800

    integration-12.3

---
+++ src/etc/profile.d/bookworm-compatability-mode.sh
@@ -0,0 +1,4 @@
+export XDG_DATA_DIRS
+APP_ROOT="/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/bookworm-env/usr/share"
+XDG_DATA_DIRS="${APP_ROOT}:/usr/local/share:/usr/share"
+export APP_ROOT
