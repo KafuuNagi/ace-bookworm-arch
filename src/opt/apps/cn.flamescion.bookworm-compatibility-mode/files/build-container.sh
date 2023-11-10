b826b4c67ca8b7cc374737a30b87cd463dacea47
shenmo <jifengshenmo@outlook.com>
2023-11-10 15:51:10 +0800

    unite-ace-host-integration

--- src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
+++ src/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/build-container.sh
@@ -25,23 +25,16 @@ fi
 cd "`dirname $0`"
 sudo debootstrap --arch=${ARCH} bookworm ./bookworm-env https://mirrors.ustc.edu.cn/debian/
 
-pushd bookworm-env
-
-
-gitee_api_url="https://gitee.com/api/v5/repos/amber-compatability-environment/ace-host-integration/releases/latest"
-resp="$(curl -s $gitee_api_url)"
-VERSION_GITEE="$(jq -r '.tag_name' <<<$resp | sed "s/.*V\([^_]*\).*/\1/g")"
-echo "$VERSION_GITEE"
-
-sudo wget https://gitee.com/amber-compatability-environment/ace-host-integration/releases/download/${VERSION_GITEE}/ace-host-integration_${VERSION_GITEE}_all.deb
-
-popd
-
-sudo systemd-nspawn -D bookworm-env apt install /ace-host-integration_${VERSION_GITEE}_all.deb -y
-sudo rm bookworm-env/ace-host-integration_${VERSION_GITEE}_all.deb
-
 
 sudo rm -rf bookworm-env/var/cache/apt/archives/*.deb
 
 sudo tar -I 'xz -T0' -cvf bookworm-env.tar.xz bookworm-env/*
 sudo rm -rf bookworm-env
+
+pushd flamescion-container-tools/ace-host-integration
+
+dpkg-deb -Z xz -b . ../ace-host-integration.deb
+
+popd
+
+
