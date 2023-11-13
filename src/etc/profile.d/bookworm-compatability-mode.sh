c26bf5aa0670dd224bac3511e233454767f1a5a8
RX6900XT <qq482961502@gmail.com>
2023-11-13 10:57:35 +0000

    update src/etc/profile.d/bookworm-compatability-mode.sh.
    Signed-off-by: RX6900XT <qq482961502@gmail.com>

--- src/etc/profile.d/bookworm-compatability-mode.sh
+++ src/etc/profile.d/bookworm-compatability-mode.sh
@@ -9,8 +9,7 @@ if [ -z "$XDG_DATA_DIRS" ]; then
 fi
 
 # Desktop files (used by desktop environments within both X11 and Wayland) are
-# looked for in XDG_DATA_DIRS; make sure it includes the relevant directory for
-# snappy applications' desktop files.
+# looked for in XDG_DATA_DIRS; make sure it includes the relevant directory for ACE
 ACE_path="/opt/apps/cn.flamescion.bookworm-compatibility-mode/files/bookworm-env/usr/share/"
 if [ -n "${XDG_DATA_DIRS##*${ACE_path}}" ] && [ -n "${XDG_DATA_DIRS##*${ACE_path}:*}" ]; then
     export XDG_DATA_DIRS="${ACE_path}:${XDG_DATA_DIRS}"
